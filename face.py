92303258718e462289542208828e2915bff444b4
Martin Baroody <martin.baroody@mail.utoronto.ca>
2019-01-19 14:48:44 -0500

    Gang no squad

---
+++ face.py
@@ -0,0 +1,34 @@
+import requests
+
+def detect_face(image, local=False):
+    url = 'https:///canadacentral.api.cognitive.microsoft.com/face/v1.0/detect'
+    headers = None
+    data = None
+    json = None
+    if not local:
+        headers = {'Content-Type': 'application/json'}
+        json = {'url': image}
+    else:
+        headers = {'Content-Type': 'application/octet-stream'}
+        data = open(image, 'rb').read()
+
+    params = {
+        'returnFaceId': True,
+        'returnFaceLandmarks': False,
+        'returnFaceAttributes': 'emotion',
+    }
+
+    return requests.post(url, headers=headers, params=params, json=json, data=data)
+
+
+def identify_face(face_ids, person_group_id=None, max_candidates=50, threshold=None):
+    url = 'https://canadacentral.api.cognitive.microsoft.com/face/v1.0/identify'
+    json = {
+        'personGroupId': person_group_id,
+        'largePersonGroupId': None,
+        'faceIds': face_ids,
+        'maxNumOfCandidatesReturned': max_candidates,
+        'confidenceThreshold': threshold,
+    }
+
+    return requests.post(url, json=json)
