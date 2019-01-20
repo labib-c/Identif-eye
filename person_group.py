26b3d99a2e16e392375950466c0f2e3335463d9c
Martin Baroody <martin.baroody@mail.utoronto.ca>
2019-01-20 03:54:22 -0500

    a lil change

--- person_group.py
+++ person_group.py
@@ -34,6 +34,16 @@ def trainingStatus(person_group_id):
 	url = 'https://canadacentral.api.cognitive.microsoft.com/face/v1.0/persongroups/{}/training'.format(person_group_id)
 	return requests.get(url, headers=headers)
 
+def test_person(image, threshold, group_name='group1'):
+	Ids = [face.detect_face(image).json()[0]['faceId']]
+
+	candidates = face.identify_face(Ids, group_name).json()[0].get('candidates')
+
+	for p in candidates:
+		print(person.getPerson(group_name, p.get('personId')).json())
+
+	return face.detect_wanted(candidates, threshold)
+
 def main():
 	
 	# print(create("group1"))
@@ -45,31 +55,35 @@ def main():
 	# person_id = new_person.json().get('personId')
 	# person_id2 = new_person2.json().get('personId')
 
-	image = 'https://goo.gl/zFdk6i' #Saksham
-	image2 = "https://goo.gl/j4sUip" #Labib
-	image3 = "https://goo.gl/M65gQU" #Labib2
-	image4 = "https://goo.gl/JxWSAK" #Martin
-	image5 = 'https://bit.ly/2sCacna' #Saksham
+	# '''for testing links:	'''
+	# image = 'https://goo.gl/zFdk6i' #Saksham
+	# image2 = "https://goo.gl/j4sUip" #Labib
+	# image3 = "https://goo.gl/M65gQU" #Labib2
+	# image4 = "https://goo.gl/JxWSAK" #Martin
+	# image5 = 'https://bit.ly/2sCacna' #Saksham
 
 
 	# person.addFace(image, 'group1', person_id2)
 	# person.addFace(image2, 'group1', person_id)
 	# person.addFace(image3, 'group1', person_id)
 
-	Ids = [ face.detect_face(image5).json()[0]['faceId'] ]
-	print(Ids, "\n")
+	# Ids = [ face.detect_face(image5).json()[0]['faceId'] ]
+	# print(Ids, "\n")
+	#
+	# print(train("group1"))
+	# print(trainingStatus("group1"))
+	# candidates = face.identify_face(Ids, "group1").json()[0].get('candidates')
+	#
+	# print(candidates, "\n")
+	#
+	# for p in candidates:
+	# 	print(person.getPerson("group1", p.get('personId')).json())
+	#
+	# print('\n')
+	# print(face.detect_wanted(candidates, 0.4))
 
-	print(train("group1"))
-	print(trainingStatus("group1"))
-	candidates = face.identify_face(Ids, "group1").json()[0].get('candidates')
-
-	print(candidates, "\n")
-
-	for p in candidates:
-		print(person.getPerson("group1", p.get('personId')).json())
-
-	print('\n')
-	print(face.detect_wanted(candidates, 0.4))
+	goodguy_path = 'https://bit.ly/2sCacna'
+	print(test_person(goodguy_path, 0.4))
 
 
 if __name__ == '__main__':
