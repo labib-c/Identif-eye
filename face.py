20065c04b652ef604e331c69bb94a92de5b69f3f
Martin Baroody <martin.baroody@mail.utoronto.ca>
2019-01-20 03:57:18 -0500

    yo

--- face.py
+++ face.py
@@ -3,51 +3,46 @@ import requests
 subscription_key = "39f85cf12c644292a3dcd910ee8e61ea"
 headers = {'Ocp-Apim-Subscription-Key': subscription_key}
 
-def detect_face(image, local=False):
-    url = 'https://canadacentral.api.cognitive.microsoft.com/face/v1.0/detect'
-    data = None
-    json = None
-    if not local:
-        headers['Content-Type'] = 'application/json'
-        json = {'url': image}
-    else:
-        headers['Content-Type'] = 'application/octet-stream'
-        data = open(image, 'rb').read()
+def detect_face(image):
+	url = 'https://canadacentral.api.cognitive.microsoft.com/face/v1.0/detect'
 
-    params = {
-        'returnFaceId': True,
-        'returnFaceLandmarks': False,
-        'returnFaceAttributes': 'emotion',
-    }
+	headers['Content-Type'] = 'application/json'
+	json = {'url': image}
 
-    return requests.post(url, headers=headers, params=params, json=json, data=data)
+	params = {
+		'returnFaceId': True,
+		'returnFaceLandmarks': False,
+		'returnFaceAttributes': 'emotion',
+	}
+
+	return requests.post(url, headers=headers, params=params, json=json, data=None)
 
 
 def identify_face(face_ids, person_group_id=None, max_candidates=50, threshold=None):
-    url = 'https://canadacentral.api.cognitive.microsoft.com/face/v1.0/identify'
-    json = {
-        'personGroupId': person_group_id,
-        'largePersonGroupId': None,
-        'faceIds': face_ids,
-        'maxNumOfCandidatesReturned': max_candidates,
-        'confidenceThreshold': threshold,
-    }
+	url = 'https://canadacentral.api.cognitive.microsoft.com/face/v1.0/identify'
+	json = {
+		'personGroupId': person_group_id,
+		'largePersonGroupId': None,
+		'faceIds': face_ids,
+		'maxNumOfCandidatesReturned': max_candidates,
+		'confidenceThreshold': threshold,
+	}
 
-    return requests.post(url, headers=headers, json=json)
+	return requests.post(url, headers=headers, json=json)
 
 
 def detect_wanted(json_dict_list, threshold):
-    # return criminal/not (True/False), confidence, and personId
-    if len(json_dict_list) == 0:
-        return False, None, None
+	# return criminal/not (True/False), confidence, and personId
+	if len(json_dict_list) == 0:
+		return False, None, None
 
-    for item in json_dict_list:
-        if 'candidates' in item:
-            if item['candidates']['confidence'] >= threshold:
-                return True, item['candidates']['confidence'], item['candidates']['personId']
-        else:
-            if item['confidence'] >= threshold:
-                return True, item['confidence'], item['personId']
+	for item in json_dict_list:
+		if 'candidates' in item:
+			if item['candidates']['confidence'] >= threshold:
+				return True, item['candidates']['confidence'], item['candidates']['personId']
+		else:
+			if item['confidence'] >= threshold:
+				return True, item['confidence'], item['personId']
 
 
-    return False, None, None
+	return False, None, None
